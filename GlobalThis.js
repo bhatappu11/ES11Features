65d961dc30106bde7b408f33112f1aa2d109fde3
Arpitha Bhat <arpithabht12@gmail.com>
2021-12-14 07:29:40 +0530

    [Arpitha]ADDED: demonstration of globalThis

---
+++ GlobalThis.js
@@ -0,0 +1,11 @@
+// // In a browser
+// window == globalThis // true
+
+// In node.js
+global == globalThis // true
+
+//console.log(window);
+//console.log(self);
+//console.log(frames);
+console.log(this);
+console.log(globalThis)
